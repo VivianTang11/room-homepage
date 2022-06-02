771af1c5a822ee4f9d2cfd4e890a23a2e85d81bf
VivianTang11 <vivian.tang@hotmail.se>
2022-06-02 09:57:31 +0200

    Completed Header

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+// Navbar toggler
 const toggleImg = document.querySelector('.toggle-img');
 const toggleBtn = document.querySelector('.toggle-btn');
-
-
 toggleBtn.addEventListener("click", ()=> {
     if (toggleImg.getAttribute('src') === './images/icon-hamburger.svg') {
         toggleImg.setAttribute('src','./images/icon-close.svg')
+        document.querySelector('.navbar').classList.toggle('active')
         document.querySelector('nav').classList.toggle('active')
+        document.querySelector('.logo-img').classList.toggle('active')
     } else {
         toggleImg.setAttribute('src', './images/icon-hamburger.svg')
+        document.querySelector('.navbar').classList.toggle('active')
         document.querySelector('nav').classList.toggle('active')
-
+        document.querySelector('.logo-img').classList.toggle('active')
     }
 });
+
+
